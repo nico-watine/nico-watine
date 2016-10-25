6403b997c62ba189448b8f32ade020937c1abbb3
Nico Watine <nico@studioncreations.com>
2016-10-25 10:25:53 -0500

    Specified portfolio link font size
    Declared it as h4 so as to keep font consistent

--- src/pages/index.php
+++ src/pages/index.php
@@ -19,7 +19,7 @@ body-class:
 					<td><h3 itemprop="description">A <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, <span itemprop="jobTitle">web designer</span> who thinks he can play the saxophone and take pretty pictures.</h3>
 					<hr>
 					<h2 class="title">Portfolio</h2>
-					<a href="http://studioncreations.com" itemprop="url" target="_blank">http://studioncreations.com</a></td>
+					<h4><a href="http://studioncreations.com" itemprop="url" target="_blank">http://studioncreations.com</a></h4></td>
 				</tr>
 			</tbody>
 		</table>
