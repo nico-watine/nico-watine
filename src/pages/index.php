b06b339fbf9605418687ff4838af2f232332b3a6
Nico Watine <nico@studioncreations.com>
2017-01-21 19:18:22 -0600

    New text entry
    Need to schemify it and figure out the proper h1… tags

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,15 +11,15 @@ body-class: index
 <section class="row">
 	<div itemscope itemtype="http://schema.org/Person" class="bio-summary contrast">
 		<h1 itemprop="name" class="title">Nico Watine</h1>
-		<h4 class="serif"><strong><i>\ Nee-ko waw-Teen \</i></strong></h4>
+		<!-- <h4 class="serif"><strong><i>\ Nee-ko waw-Teen \</i></strong></h4> -->
 		<table class="transparent">
 			<tbody>
 				<tr>
-					<td><h3>1 <span class="float-right">:</span></h3></td>
-					<td><h3 itemprop="description">A <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, <span itemprop="jobTitle">web designer</span> who thinks he can play the saxophone and take pretty pictures.</h3>
+					<!-- <td><h3>1 <span class="float-right">:</span></h3></td> -->
+					<td><h3>I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</h3><!-- <h3 itemprop="description">A <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, <span itemprop="jobTitle">web designer</span> who thinks he can play the saxophone and take pretty pictures.</h3> -->
 					<hr>
 					<h2 class="title">Portfolio</h2>
-					<h4><a href="http://studioncreations.com" id="snc-link" itemprop="url">http://studioncreations.com</a></h4></td>
+					<h3><a href="http://studioncreations.com" id="snc-link" itemprop="url">http://studioncreations.com</a></h3></td>
 				</tr>
 			</tbody>
 		</table>
