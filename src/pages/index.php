bc30716c44001bc3e3d8e00560be07ca59acb901
Nico Watine <nico@studioncreations.com>
2017-12-27 12:30:52 -0600

    Insert plain block elements

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,5 +8,11 @@ og-image:
 body-id: index
 body-class: index
 ---
+<div class="row">
+	<h1>Nico Watine</h1>
+	<p>I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
+	<h3>Portfolio</h3>
+	<p>http://studioncreations.com</p>
+</div>
 <div class="gradient-bg"></div>
 <div class="bg-img"></div>
