7dbae7e9c21bdf9f0fab3e96432093532886bb91
Nico Watine <nico@studioncreations.com>
2017-12-27 14:16:27 -0600

    Specify no margin for h1 and h3

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,9 +9,9 @@ body-id: index
 body-class: index
 ---
 <div class="row">
-	<h1><strong>Nico Watine</strong></h1>
+	<h1 class="margin-0"><strong>Nico Watine</strong></h1>
 	<p class="h4" style="max-width: 600px;">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
-	<h3><strong>Portfolio</strong></h3>
+	<h3 class="margin-0"><strong>Portfolio</strong></h3>
 	<p class="h4">http://studioncreations.com</p>
 </div>
 <div class="gradient-bg"></div>
