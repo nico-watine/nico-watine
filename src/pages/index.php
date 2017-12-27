c62d47e51a8cf427e5d532fcd68a79d0225a76e5
Nico Watine <nico@studioncreations.com>
2017-12-27 12:29:06 -0600

    Clear <table> markup to replace

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,20 +8,5 @@ og-image:
 body-id: index
 body-class: index
 ---
-<section class="row">
-	<div itemscope itemtype="http://schema.org/Person" class="bio-summary contrast">
-		<h1 itemprop="name" class="title">Nico Watine</h1>
-		<table class="transparent">
-			<tbody>
-				<tr>
-					<td><h3 itemprop="description">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</h3>
-					<hr>
-					<h2 class="title">Portfolio</h2>
-					<h3><a href="https://studioncreations.com" id="snc-link" itemprop="url">https://studioncreations.com</a></h3></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</section>
 <div class="gradient-bg"></div>
 <div class="bg-img"></div>
