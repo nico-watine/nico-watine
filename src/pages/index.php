f9719748a0e725bc0bb1dd1da44a41279a701f94
Nico Watine <nico@studioncreations.com>
2017-12-27 14:11:27 -0600

    Move inline text styles to index sass

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,10 +9,10 @@ body-id: index
 body-class: index
 ---
 <div class="row">
-	<h1 style="color: #fff; text-shadow: 0 2px 3px #000;"><strong>Nico Watine</strong></h1>
-	<p style="color: #fff; text-shadow: 0 2px 3px #000; max-width: 600px;">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
-	<h3 style="color: #fff; text-shadow: 0 2px 3px #000;"><strong>Portfolio</strong></h3>
-	<p style="color: #fff; text-shadow: 0 2px 3px #000;">http://studioncreations.com</p>
+	<h1><strong>Nico Watine</strong></h1>
+	<p style="max-width: 600px;">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
+	<h3><strong>Portfolio</strong></h3>
+	<p>http://studioncreations.com</p>
 </div>
 <div class="gradient-bg"></div>
 <div class="bg-img"></div>
