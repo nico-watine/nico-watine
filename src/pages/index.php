a743bcd0594e0c509e04d4581fd115d8400b4585
Nico Watine <nico@studioncreations.com>
2017-12-27 14:12:41 -0600

    Add .h4 class to p

--- src/pages/index.php
+++ src/pages/index.php
@@ -10,9 +10,9 @@ body-class: index
 ---
 <div class="row">
 	<h1><strong>Nico Watine</strong></h1>
-	<p style="max-width: 600px;">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
+	<p class="h4" style="max-width: 600px;">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
 	<h3><strong>Portfolio</strong></h3>
-	<p>http://studioncreations.com</p>
+	<p class="h4">http://studioncreations.com</p>
 </div>
 <div class="gradient-bg"></div>
 <div class="bg-img"></div>
