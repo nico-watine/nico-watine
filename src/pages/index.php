7df4b5138d77fce06c4c2bf4a6983e3911d2eb25
Nico Watine <nico@studioncreations.com>
2018-01-07 01:29:21 -0600

    Added schema info from original doc

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,12 +9,12 @@ body-id: index
 body-class: index
 ---
 <div class="row flex heightWrapper">
-	<div class="pageContent columns flex">
-		<h1 class="margin-0"><strong>Nico Watine</strong></h1>
-		<p class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere.</p>
+	<div itemscope itemtype="http://schema.org/Person" class="pageContent columns flex">
+		<h1 itemprop="name" class="margin-0"><strong>Nico Watine</strong></h1>
+		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</p>
 		<br>
 		<h3 class="margin-0"><strong>Portfolio</strong></h3>
-		<p class="h4">http://studioncreations.com</p>
+		<p itemprop="url" class="h4">http://studioncreations.com</p>
 	</div>
 </div>
 <div class="gradient-bg"></div>
