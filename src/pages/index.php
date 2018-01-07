c8351bd1910a509105c09754cc816a1ecfc0cfe4
Nico Watine <nico@studioncreations.com>
2018-01-07 14:52:31 -0600

    Wrap the br in a span so they register in FF

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,9 +11,9 @@ body-class: index
 <div class="row flex heightWrapper">
 	<div itemscope itemtype="http://schema.org/Person" class="pageContent columns flex">
 		<h1 class="margin-0"><strong><span class="h3 block">Hi, I&rsquo;m</span> <span itemprop="name">Nico Watine</span></strong></h1>
-		<br>
+		<span><br></span>
 		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</p>
-		<br>
+		<span><br></span>
 		<h3 class="margin-0"><strong>Portfolio</strong></h3>
 		<a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a>
 	</div>
