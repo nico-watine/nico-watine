d0d25bb5a55bf1eee8692dd8e2d8135d1162c603
Nico Watine <nico@studioncreations.com>
2018-01-07 14:35:34 -0600

    Add intro hello, via separate tag

--- src/pages/index.php
+++ src/pages/index.php
@@ -10,7 +10,9 @@ body-class: index
 ---
 <div class="row flex heightWrapper">
 	<div itemscope itemtype="http://schema.org/Person" class="pageContent columns flex">
-		<h1 itemprop="name" class="margin-0"><strong>Nico Watine</strong></h1>
+		<h3 class="margin-0" style="line-height: 1;"><strong>Hi, I&rsquo;m</strong></h3>
+		<h1 itemprop="name" class="margin-0" style="line-height: 1;"><strong>Nico Watine</strong></h1>
+		<br>
 		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</p>
 		<br>
 		<h3 class="margin-0"><strong>Portfolio</strong></h3>
