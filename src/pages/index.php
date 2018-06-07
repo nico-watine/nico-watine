f17ecf053701eefe7bec1c1c9561ac897146b147
Nico Watine <nico@studioncreations.com>
2018-06-07 04:05:31 -0500

    Good progress on min-height stuff

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,18 +8,21 @@ og-image:
 body-id: index
 body-class: index
 ---
-<div class="row flex heightWrapper" style="padding-top: 50px;">
-	<div itemscope itemtype="http://schema.org/Person" class="pageContent columns flex">
+<div class="height-wrapper" style="min-height: 100%; position: relative;">
+<div class="row" style="padding: 50px 0 150px;">
+	<div itemscope itemtype="http://schema.org/Person" class="columns">
 		<h1 class="margin-0"><strong><span class="h3 block">Hi, I&rsquo;m</span> <span itemprop="name">Nico Watine</span></strong></h1>
 		<span><br></span>
 		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</p>
-		<span><br></span>
-		<h3 class="margin-0"><strong>Portfolio</strong></h3>
-		<a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a>
+		<!-- <span><br></span> -->
+		<!-- <h3 class="margin-0"><strong>Portfolio</strong></h3> -->
+		<!-- <a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a> -->
 	</div>
 </div>
-<aside class="skylineDetail" style="position: absolute; bottom: 0; max-width: 800px; width: 100%;">
+<aside class="skylineDetail" style="position: absolute; bottom: 0;">
+	<p style="position: absolute; bottom: 0; width: 100%; text-align: right; padding-right: 30px;">Lorem ipsum dolor</p>
 	<img src="/img/index/background/houston-skyline-detail.png" alt="Houston TX Skyline">
 </aside>
+</div>
 <!-- <div class="gradient-bg"></div> -->
 <!-- <div class="bg-img"></div> -->
