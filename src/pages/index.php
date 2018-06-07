fff735b6dabb94e9e0a65b80391e6fb75aa3f106
Nico Watine <nico@studioncreations.com>
2018-06-07 04:32:22 -0500

    Erase nulled lines

--- src/pages/index.php
+++ src/pages/index.php
@@ -14,18 +14,13 @@ body-class: index
 		<h1 style="line-height: 1.1;"><strong><span class="h3 block">Hi, I&rsquo;m</span> <span style="line-height: 1;" itemprop="name">Nico Watine</span></strong></h1>
 		<span><br></span>
 		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span>, and am available for hire anywhere.</p>
-		<!-- <span><br></span> -->
-		<!-- <h3 class="margin-0"><strong>Portfolio</strong></h3> -->
-		<!-- <a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a> -->
 	</div>
 </div>
 <aside class="skylineDetail" style="position: absolute; bottom: 0; width: 100%; height: 220px;">
 	<div class="prose" style="padding: 0 15px 15px; position: absolute; bottom: 0; width: 100%; text-align: right;">
 		<h3 class="margin-0"><strong>Portfolio</strong></h3>
 		<a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a>
-		<!-- <p class="h4">Lorem ipsum dolor</p> -->
 	</div>
-	<!-- <img src="/img/index/background/houston-skyline-detail.png" alt="Houston TX Skyline"> -->
 </aside>
 </div>
 <!-- <div class="gradient-bg"></div> -->
