e150a8c1d0ef0282cd235c0b140d5b3623de3eee
Nico Watine <nico@studioncreations.com>
2018-06-07 01:33:41 -0500

    Place the skyline image

--- src/pages/index.php
+++ src/pages/index.php
@@ -18,8 +18,8 @@ body-class: index
 		<a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a>
 	</div>
 </div>
-<aside class="skylineDetail" style="position: absolute; bottom: 0; height: 100px; background: red; max-width: 800px; width: 100%;">
-	
+<aside class="skylineDetail" style="position: absolute; bottom: 0; max-width: 800px; width: 100%;">
+	<img src="/img/index/background/houston-skyline-detail.png" alt="Houston TX Skyline">
 </aside>
 <!-- <div class="gradient-bg"></div> -->
 <!-- <div class="bg-img"></div> -->
