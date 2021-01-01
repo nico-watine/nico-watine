e0566fd7b56bd7cb7cd6e0134a82d951cbbbbdc1
Nico Watine <nico@studioncreations.com>
2021-01-01 10:35:33 -0600

    Update components/elements directory

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,14 +11,14 @@ body-class: index
 <div class="heightWrapper">
 <div class="row">
 	<div itemscope itemtype="http://schema.org/Person" class="columns prose">
-		<h1 style="line-height: 1.1;"><strong><span class="h3 block">Hi, I’m</span> <span style="line-height: 1;" itemprop="name">Nico Watine</span></strong></h1>
+		<h1 style="line-height: 1.1;"><strong><span class="h3 display-block">Hi, I’m</span> <span style="line-height: 1;" itemprop="name">Nico Watine</span></strong></h1>
 		<span><br></span>
 		<p itemprop="description" class="h4 summary">I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in <strong><span itemprop="addressLocality">Houston</span>, <span itemprop="addressRegion">TX</span></strong>, and am available for hire anywhere.</p>
 	</div>
 </div>
 <aside class="skylineDetail">
 	<div class="prose">
-		<h3 class="margin-0"><strong>Portfolio &amp; Contact</strong></h3>
+		<h3 class="m-0"><strong>Portfolio &amp; Contact</strong></h3>
 		<a itemprop="url" href="https://studioncreations.com/" class="h4 colophonLink">https://studioncreations.com</a>
 	</div>
 </aside>
